5b1f92a7cba1907a162636d249e14f1e45acf9df
williamantoniozup <william.antonio@zup.com.br>
2020-09-27 13:40:53 -0300

    making http requests

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-console.log('Starting')
+const request = require('postman-request')
+// console.log('request', request);
 
-setTimeout(() => {
-  console.log('2 second timer')
-}, 2000)
+const url = 'http://api.weatherstack.com/current?access_key=2389ca1cebb708e5a4db67f0110b176a&query=37.8267,-122.4233&units=f'
 
-setTimeout(() => {
-  console.log('0 Second Timer')
-}, 0)
+request({url: url, json: true}, (error, response) => {
+  // console.log('response: ', response);
+  // const data = JSON.parse(response.body)
+  // console.log(data);
+  // console.log('Current: ' + data.current.temperature);
+  const {current} = response.body;
+  const {temperature, feelslike, weather_descriptions} = current;
+  console.log( weather_descriptions[0] + '. It is a currently ' + temperature + ' degress out. It feels like ' + feelslike + ' degress out.')
 
-console.log('Stopping')
+})
+
