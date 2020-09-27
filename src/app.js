f680a145e03f47c6573c23decf8e7377764d3a63
williamantoniozup <william.antonio@zup.com.br>
2020-09-27 17:19:33 -0300

    geocoding request api mapbox

--- src/app.js
+++ src/app.js
@@ -11,6 +11,22 @@ request({url: url, json: true}, (error, response) => {
   const {current} = response.body;
   const {temperature, feelslike, weather_descriptions} = current;
   console.log( weather_descriptions[0] + '. It is a currently ' + temperature + ' degress out. It feels like ' + feelslike + ' degress out.')
-
 })
 
+// Geocoding
+// Address -> lat/long -> Weather
+
+const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoid2lsbGlhbXNjaGVpY2hlciIsImEiOiJja2ZsYzZ3anYxNTdyMnhxZWhodmdqMHVtIn0.mlF0L9YLl8eGGGBEL-5h9A'
+
+
+
+request({url: url2, json: true}, (error, response) => {
+  // console.log('response: ', response.body);
+  const { features } = response.body
+  const {center} = features[0];
+  const latitude = center[1]
+  const {center: centerLong} = features[0];
+  const longitude = centerLong[0]
+  console.log('latitude', latitude);
+  console.log('longitude', longitude);
+})
