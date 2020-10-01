30ae42e4ec6d2f3cc4a0645f27873b39e0f2cdf1
williamantoniozup <william.antonio@zup.com.br>
2020-09-30 21:12:32 -0300

    serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,22 +3,36 @@ const express = require('express')
 const app = express()
 // app.com
 app.get('', (req,res) => {
-  res.send('Hello Express!')
+  res.send('<h1>Weather</h1>')
 })
 
 // app.com/help
 app.get('/help', (req,res) => {
-  res.send('Help page!')
+  res.send({
+    name: 'William',
+    age: 28
+  })
 })
 
 // app.com/about
 app.get('/about', (req,res) => {
-  res.send('About page!')
+  res.send('<h1>About</h1>')
 })
 
 // app.com/weather
 app.get('/weather', (req,res) => {
-  res.send('Weather page!')
+  res.send({
+    forecast: 'It is snowing',
+    location: 'Uberlandia'
+  })
+})
+
+app.get('/isabela', (req,res) => {
+  res.send({
+    name: 'Isabela Scheicher',
+    age: 26,
+    descripton: 'Muito gostosa!'
+  })
 })
 
 app.listen(3000, () => {
