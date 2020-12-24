9f4f4bc820f38cc17e853cd0bd0b3e7266417df1
williamhsa <whsa2021>
2020-12-24 18:26:30 -0300

    feat: create view information weather for user

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -7,19 +7,45 @@ const getPuzzle = () => {
 }
 
 
-const getWeather = () => {
-  const address = 'Campinas'
-  fetch(`http://localhost:3000/weather?address=${address}`).then( res => {
+const getWeather = locationIn => {
+  // const address = 'Campinas'
+
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
+
+  fetch(`http://localhost:3000/weather?address=${locationIn}`).then( res => {
     res.json().then( data => {
-      console.log(`\n In ${data.location} is ${data.forecast}\n`)
+      if (data.error) {
+        console.log(data.error);
+        messageOne.textContent = data.error
+        messageTwo.textContent = ''
+      } else {
+        console.log(`\n In ${data.location} is ${data.forecast}\n`)
+        messageOne.textContent = ''
+        messageTwo.textContent = `\n In ${data.location} is ${data.forecast}\n`
+      }
     })
   }).catch(err => console.log(err))
 }
 
 
 console.log('Client side javascript file is loaded!');
-getWeather()
+// getWeather()
 getPuzzle()
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'From Javascript'
+
+weatherForm.addEventListener('submit', e => {
+  e.preventDefault() //previne o comportamento padrao que é atualizar o navegador.
+  const location = search.value
+  // console.log('works!');
+  getWeather(location)
+})
 
 
 
